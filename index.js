57b94baf496b186bf01cd32ec904bf9a44914e25
abideen11 <abideenkareem17@gmail.com>
2019-08-02 17:46:04 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(celebrant,birthday) {
+    let celebrants = [];
+    for (var i = 0; i < celebrant.length; i++) {
+        celebrants.push(`Thank you, ${celebrant[i]}, for the wonderful ${birthday} gift!`);
+    }
+    return celebrants;
+}
+function countdown(n) {
+    while (n >= 0) {
+        console.log(n);
+        n--;
+    }
+}
+
