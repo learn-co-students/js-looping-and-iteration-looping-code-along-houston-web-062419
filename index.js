def223c1f15db4858063d26fb939f7ba77cce065
Varvara Babkova <varvara.munday@gmail.com>
2019-08-05 20:16:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards (names, cause)
+{
+  let cards = [];
+  for (let i = 0; i < names.length; i++)
+  {
+    cards.push(`Thank you, ${names[i]}, for the wonderful ${cause} gift!`);
+  }
+  return cards;
+}
+
+function countdown(num)
+{
+  while (num >= 0) {
+    console.log(num--);
+  }
+}
